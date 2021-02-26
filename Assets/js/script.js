76e32994163dbe6fdee85df4c12193921a209d05
Amal Janabayev <amalj2426@gmail.com>
2021-02-25 16:22:11 -0800

    Updated version for resubmission

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,68 +18,20 @@ $(document).ready(function(){
         $("#myProjects").css({backgroundColor:'transparent'})
     })
 
-    var firstColumn = $("#firstColumn");
-    var secondColumn = $("#secondColumn");
-
-    var lostNoMore = $("<div class='card projectShowcase' id='lostNoMore'>").css({height:'350px',display:'block',marginTop:'10%', backgroundImage:'url(./Assets/css/Pictures/lnm.png)'})
-    var lostNomoreTitle = $("<h2 class='projectTitles' id='lostNoMoreTitle'>").text('LostNoMore - Interactive travelling guide').css({backgroundColor:'#4666FF',color:'white',borderRadius:'5%'})
-    var lnmLink = $("<span class='projectlinks'><a href='https://obrienceob.github.io/LostNoMore/'><p >Deployed link -->").css({color:'white'})
-    var githublnmLink = $("<span class='projectlinks'><a href=' https://github.com/obrienceob/LostNoMore'><p >Repo link -->").css({color:'white'})
-    lostNoMore.append(lnmLink,githublnmLink).hide();
-    firstColumn.append(lostNoMore);
-    $("#lostNoMore").mouseover(function(){
-        lostNomoreTitle.slideDown(350).stop()
-    }).mouseout(function(){
-        lostNomoreTitle.hide()
-    })
-
-    var passwordProject = $("<div class='card projectShowcase' id='passwordProject'>").css({height:'350px', display:'block',backgroundImage:'url(./Assets/css/Pictures/password.png)'})
-    var passwordProjectTitle = $("<h2 class='projectTitles' id='passwordProjectTitle'>").text('Password Generator')
-    var pswLink = $("<span class='projectlinks'><a href=' https://amal31497.github.io/Homework-3/'><p >Deployed link -->")
-    var githubpswLink = $("<span class='projectlinks'><a href='https://github.com/Amal31497/Homework-3'><p >Repo link -->")
-    passwordProjectTitle.append(pswLink,githubpswLink).hide();
-    passwordProject.append(passwordProjectTitle);
-    firstColumn.append(passwordProject)
-    $("#passwordProject").mouseover(function(){
-        passwordProjectTitle.slideDown(350).stop()
-    }).mouseout(function(){
-        passwordProjectTitle.hide()
-    })
-
-    var weather = $("<div class='card projectShowcase' id='weatherProject'>").css({height:'350px', display:'block', backgroundImage:'url(./Assets/css/Pictures/weather.png)'})
-    var weatherProjectTitle = $("<h2 class='projectTitles' id='weatherProject'>").text('Weather Dashboard')
-    var wLink = $("<span class='projectlinks'><a href='https://amal31497.github.io/Weather-Dashboard/'><p >Deployed link -->")
-    var githubwLink = $("<span class='projectlinks'><a href='https://github.com/Amal31497/Weather-Dashboard'><p >Repo link -->")
-    weatherProjectTitle.append(wLink,githubwLink).hide();
-    weather.append(weatherProjectTitle);
-    secondColumn.append(weather);
-    $("#weatherProject").mouseover(function(){
-        weatherProjectTitle.slideDown(350).stop()
-    }).mouseout(function(){
-        weatherProjectTitle.hide()
-    })
-
-    var schedule = $("<div class='card projectShowcase' id='scheduleProject'>").css({height:'350px', display:'block', backgroundImage:'url(./Assets/css/Pictures/schedule.png)'})
-    var scheduleProjectTitle = $("<h2 class='projectTitles' id='scheduleProject'>").text('Scheduling tool');
-    var schLink = $("<span class='projectlinks'><a href=' https://amal31497.github.io/Homework-5/'><p >Deployed link -->")
-    var githubschLink = $("<span class='projectlinks'><a href=' https://github.com/Amal31497/Homework-5'><p >Repo link -->")
-    scheduleProjectTitle.append(schLink,githubschLink).hide();
-    schedule.append(scheduleProjectTitle);
-    secondColumn.append(schedule);
     $("#lastMessage").click(function(){
         $("#lastMessage").slideUp(400)
     })
     var contactPage = $("<div class='columns' id='contactPage'>")
-    var contactInputs = $("<div class='column' id='message-2'><h3>Your name: <input class='inputFields' id='yourName'><h3>Your phone #: <input class='inputFields' id='yourNum'><br><h3>Your email: <input class='inputFields' id='yourEmail'><br><h3>Leave me a message: <input class='inputFields' id='yourMessage'><br><br><h2>Send it").css({float:'right'})
-    var contactMessages = $("<div class='column' id='message-1'><h2 id='workWithMe'>Contact me - Let's Work Together!<h4 id='address'>Amal Janabayev<br>1630 228th St SE<br>Bothell, WA 98021<h4/>").css({float:'left'})
+    var contactInputs = $("<div class='column card' id='message-2'><h3>Your name: <input class='inputFields' id='yourName'><h3>Your phone #: <input class='inputFields' id='yourNum'><br><h3>Your email: <input class='inputFields' id='yourEmail'><br><h3>Leave me a message: <input class='inputFields' id='yourMessage'><br><br><h2>Send it").css({float:'right'})
+    var contactMessages = $("<div class='column card' id='message-1'><h2 id='workWithMe'>Contact me - Let's Work Together!<h4 id='address'>Amal Janabayev<br>1630 228th St SE<br>Bothell, WA 98021<h4/>").css({float:'left'})
     contactPage.append(contactMessages, contactInputs).css({width:'100%',margin:'0'}).hide()
     $("#contactField").append(contactPage)
 
     $("#contactField").click(function(){
         contactPage.fadeIn(300)
     })
-
-
+    // var passwordPic = $("<img class='projectShowcase' id='passwordShowcase' src='./Assets/css/Pictures/password.png'>")
+    // $("#screenshot-password").append(passwordPic)
 
 
     $("#workWithMe").click(function(){
@@ -92,9 +44,6 @@ $(document).ready(function(){
         localStorage.setItem("email",emailInput);
         localStorage.setItem("message",messageInput);
     })
-    
-
-
 })
 
 
--- README.md
+++ README.md
@@ -1,22 +1,42 @@
 # Updated-portfolio
-Description:
+## Description:
+This portfolio is a brief introduction into my professional life. I am an aspiring software developer currently taking a University of Washington full-stack coding bootcamp. So far I am interested in full-stack development. I absolutely love learning new technologies and implementing them in my work. I am looking forward to stepping into the world of technological advancement. Please give me a call at (314)-378-4296 or email me at amalj2426@gmail.com should you have any questions.
 
-Modified portfolio page. New Features - 
+
+## Modified portfolio page. New Features
       1. Mobile responsive design
       2. Multiple event listeners(including styling properties modification)
       3. Cleaner design
       4. Headshot included
 
-What I would like to fix - 
+## What I would like to fix - 
       1. Smooth scrolling
       2. Connect the back end
       3. Polish styling even further
       4. Create a better color pallette
 
-Technologies Used:
+## Technologies Used:
  a) Jquery
  b) Bulma library
  c) Web Apis, event handlers
  d) Keyframes to create a flowing effect
 
--Amal Janabayev
+## Credits:
+  [w3schools.com](w3schools.com)
+  [https://developer.mozilla.org/en-US/](https://developer.mozilla.org/en-US/)
+  []()
+
+## Links:
+[Github repo](https://github.com/Amal31497/Updated-portfolio)
+[Deployed link](https://amal31497.github.io/Updated-portfolio/)
+
+## Screenshot:
+![Screenshot](./Assets/Pictures/screenshot.png)
+
+## License: 
+
+  ##### Copyright: Amal Janabayev
+
+  ##### [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  ##### [License Link](http://www.apache.org/licenses/LICENSE-2.0)
